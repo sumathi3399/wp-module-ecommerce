c16c29f5f5c7ef44bb87d93172ebd70d7bcc7d36
Ramya krishna <ramyakrishnaimminni@gmail.com>
2023-08-29 23:28:18 +0530

    newfold chnages for payment screens

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '2ee0c797a90f90ed6969');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9844780110fe715e4700');
